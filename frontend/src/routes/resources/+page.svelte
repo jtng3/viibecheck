804bd66369f28c37276a64d0ed9a902d217bb3d9
Jaeger Tang <jaeger@tozny.com>
2023-06-15 20:42:15 -0700

    updated link descriptions in resources page

--- frontend/src/routes/resources/+page.svelte
+++ frontend/src/routes/resources/+page.svelte
@@ -17,11 +17,7 @@
 			<div class="text-primary-700 text-lg font-semibold underline">Rainn</div>
 		</a>
 		<div class="indent-4">
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			The nation's largest anti-sexual violence organization. They operate the National Sexual Assault Hotline, 800-656-HOPE
 		</div>
 	</div>
 	<div>
@@ -29,11 +25,7 @@
 			<div class="text-primary-700 text-lg font-semibold underline">Planned Parenthood</div>
 		</a>
 		<div class="indent-4">
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			Planned Parenthood delivers vital reproductive health care, sex education, and information to millions of people worldwide.
 		</div>
 	</div>
 </div>
