b41aa359f6f1aaa52892977650e8e9c3b2c8c0fb
Lee Pham <leenpham@gmail.com>
2023-06-15 20:14:28 -0700

    updates to typing

--- frontend/src/routes/resources/+page.svelte
+++ frontend/src/routes/resources/+page.svelte
@@ -1,4 +1,9 @@
-<script>
+<script lang="ts">
 </script>
+
+<svelte:head>
+	<title>viibecheck</title>
+	<meta name="resources" content="Resources Page" />
+</svelte:head>
 
 <div>Hello, this is the Resources page</div>
