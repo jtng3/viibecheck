a936a1ac16dc8640252e72f2fe7b14ad17f9319d
Lee Pham <leenpham@gmail.com>
2023-06-15 22:28:30 -0700

    updates to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -6,4 +6,19 @@
 	<meta name="About" content="About Page" />
 </svelte:head>
 
-<div>Hello, this is the About page</div>
+<div class="text-primary-600 font-bold text-lg">About</div>
+
+<div class="indent-4">
+	ViibeCheck™ is our final project for CS 465P. It was created by Jaeger Tang, Nhan Le, and Michael
+	Jenkins.
+	<br /><br />
+	Consent is an oftern overlooked issue in our society, and accusations of harrassment and assault all
+	too often go unheard, causing problematic people to continue unopposed. <br /><br />ViibeCheck™
+	was created to play a small role in remedying this. If someone has made you feel unsafe or harmed
+	you in some manner, their basic information can be submitted to our database.<br /><br /> If you
+	have concerns about an upcoming date or a new roommate, you can use their information to search
+	our database and we'll let you know whether they appear in it and a general idea of how many
+	reports there have been on them.<br /> <br />If you submit a report, we don't collect or store any
+	information about you, it is fully anonymous for your protenction. As a consequence, reports can
+	not be verified.
+</div>
