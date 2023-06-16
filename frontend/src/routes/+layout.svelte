3159b8553ad8bbca319909b11e1a94e03101db22
Lee Pham <leenpham@gmail.com>
2023-06-15 20:00:28 -0700

    updates to layout temp

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.postcss';
 </script>
 
-<div class="grid grid-cols-1 w-full h-full">
+<div class="grid grid-cols-1 min-w-screen min-h-screen bg-primary-100 items-start">
 	<div class="relative w-full flex flex-col justify-self-center justify-center">
 		<div
 			class="static bg-primary-700 h-[80px] w-full px-10 flex flex-row items-center justify-center gap-10 text-white"
@@ -14,8 +14,9 @@
 			<a class="hover:text-white/50" href="/new_report">New Report</a>
 			<a class="hover:text-white/50" href="/resources">Resources</a>
 		</div>
-		<main class="flex w-full p-10 border justify-center">
-			<div class="grid grid-cols-1 w-full max-w-6xl border">
+		<main class="flex w-full p-10 justify-center">
+			<!-- Border is only for temporary vision of how we move items within the page -->
+			<div class="grid grid-cols-1 w-full max-w-6xl border bg-white p-10">
 				<slot />
 			</div>
 		</main>
