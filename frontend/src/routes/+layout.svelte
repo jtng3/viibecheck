192559b273768bbf4a3cf15a66ab163faeff27bc
Lee Pham <leenpham@gmail.com>
2023-06-15 18:56:37 -0700

    removed imported data issues

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.postcss';
-	import './styles.css';
 </script>
 
 <div class="app">
@@ -25,23 +24,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
